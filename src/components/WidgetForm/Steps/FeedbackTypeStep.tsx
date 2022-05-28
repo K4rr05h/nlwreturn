83470031545a5afcccc7558fa0b1d40f12b652a0
K4rr05h <lucenajudymar@gmail.com>
2022-05-27 23:54:25 -0300

    Estudos 27/05

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -2,42 +2,36 @@ import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from "../Index";
 
 interface FeedbackTypeStepProps {
-    onFeedbackTypeChanged : (type: FeedbackType) => void;
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
-export function FeedbackTypeStep( props: FeedbackTypeStepProps ) {
-    return(
-        <>
+export function FeedbackTypeStep(props: FeedbackTypeStepProps) {
+  return (
+    <>
+      <header>
+        <span className="text-xl leading-6">
+            Deixe seu Feedback!
+        </span>
 
-            <header >
-                <span className="text-xl leading-6">Deixe seu Feedback!</span>
+        <CloseButton />
+      </header>
 
-                <CloseButton />
-            </header>
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:outline-none transition-all"
+              onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt}></img>
 
-            <div className="flex py-8 gap-2 w-full">
-                { 
-                    Object.entries(feedbackTypes).map(([key, value]) => {
-                                
-                        return (
-                            <button 
-                                key={key}
-                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:outline-none transition-all"
-                                onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}
-                                type="button"
-                                >
-                                    <img src={value.image.source} alt={value.image.alt}>
-                                    </img>
-                                    
-                                    <span>
-                                    {value.title}
-                                    </span>
-                                </button>
-                        );
-                                
-                    }) 
-                }
-            </div>
-        </>
-    );
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
 }
